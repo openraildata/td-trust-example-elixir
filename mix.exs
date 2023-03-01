0195b4e8484b96dab7423f94c7a8c638f6e084a1
Evelyn <git@evelyn.moe>
2023-03-01 23:32:31 +0000

    Upgrade Mammoth dependency to 0.5.3 in mix

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule NetworkRailExample.MixProject do
 
   defp deps do
     [
-      {:mammoth, "~> 0.4.1"},
+      {:mammoth, "~> 0.5.3"},
       {:jason, "~> 1.3"},
       {:tzdata, "~> 1.1"}
     ]
